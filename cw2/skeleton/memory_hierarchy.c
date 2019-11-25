6003ca223d4262bfa1e0ffc08e9139a02f816a83
Apurv Mishra <apurvmishra99@gmail.com>
2019-11-25 00:26:42 +0000

    hopefully cw2 task n2 done

--- cw2/skeleton/memory_hierarchy.c
+++ cw2/skeleton/memory_hierarchy.c
@@ -8,6 +8,21 @@
 
 /// @students: declare cache-related structures and variables here
 
+#define ADDRESS_SIZE 32
+#define CACHE_BLOCK_SIZE 16
+int NUM_BLOCKS; // cache_size/CACHE_BLOCK_SIZE
+int INDEX_BITS; // log2(NUM_BLOCKS)
+int OFFSET_BITS; // log2(CACHE_BLOCK_SIZE)
+int TAG_BITS; // ADDRESS_SIZE - INDEX_BITS - OFFSET_BITS
+
+
+struct CACHE {
+    int valid;
+    uint32_t tag;
+    int data[16];
+} cache_var;
+
+struct CACHE *SYS_CACHE;
 
 void memory_state_init(struct architectural_state* arch_state_ptr) {
     arch_state_ptr->memory = (uint32_t *) malloc(sizeof(uint32_t) * MEMORY_WORD_NUM);
@@ -17,13 +32,30 @@ void memory_state_init(struct architectural_state* arch_state_ptr) {
         memory_stats_init(arch_state_ptr, 0); // WARNING: we initialize for no cache 0
     }else {
         // CACHE ENABLED
-        assert(0); /// @students: Remove assert(0); and initialize cache
+        NUM_BLOCKS = cache_size/CACHE_BLOCK_SIZE; // cache_size/CACHE_BLOCK_SIZE
+        INDEX_BITS = ceil(log2(NUM_BLOCKS)); // log2(NUM_BLOCKS)
+        OFFSET_BITS = log2(CACHE_BLOCK_SIZE); // log2(CACHE_BLOCK_SIZE)
+        TAG_BITS = ADDRESS_SIZE - INDEX_BITS - OFFSET_BITS; // ADDRESS_SIZE - INDEX_BITS - OFFSET_BITS
 
-        /// @students: memory_stats_init(arch_state_ptr, X); <-- fill # of tag bits for cache 'X' correctly
+        arch_state_ptr->bits_for_cache_tag = TAG_BITS;
+
+        SYS_CACHE = (struct CACHE *) calloc(NUM_BLOCKS, sizeof(cache_var));
+
+        for(int i = 0; i < NUM_BLOCKS; i++)
+        {
+            SYS_CACHE[i].valid = 0;
+            SYS_CACHE[i].tag = 0;
+        }
+        memory_stats_init(arch_state_ptr, TAG_BITS);
     }
 }
 
-
+void parse_memory(int address) {
+    int byte_offset = get_piece_of_a_word(address, 0, OFFSET_BITS);
+    int index_bits = get_piece_of_a_word(address, OFFSET_BITS, INDEX_BITS);
+    int tag = get_piece_of_a_word(address, OFFSET_BITS+INDEX_BITS, TAG_BITS);
+    int addr = 0xFFFFFFF0 & address;
+}
 // returns data on memory[address / 4]
 int memory_read(int address){
     arch_state.mem_stats.lw_total++;
@@ -34,8 +66,26 @@ int memory_read(int address){
         return (int) arch_state.memory[address / 4];
     }else{
         // CACHE ENABLED
-        assert(0); /// @students: Remove assert(0); and implement Memory hierarchy w/ cache
+        int byte_offset = get_piece_of_a_word(address, 0, OFFSET_BITS) / 4;
+        int index_bits = get_piece_of_a_word(address, OFFSET_BITS, INDEX_BITS);
+        int tag = get_piece_of_a_word(address, OFFSET_BITS+INDEX_BITS, TAG_BITS);
+        int addr = 0xFFFFFFF0 & address;
 
+        if (SYS_CACHE[index_bits].valid == 1 && SYS_CACHE[index_bits].tag == tag)
+        {
+            printf("Cache hit\n");
+            arch_state.mem_stats.lw_cache_hits += 1;
+        }
+        else
+        {
+            printf("Cache miss\n");
+            SYS_CACHE[index_bits].valid = 1;
+            SYS_CACHE[index_bits].tag = tag;
+            for(int i = 0; i < CACHE_BLOCK_SIZE; i++) {
+                SYS_CACHE[index_bits].data[i] = (int) arch_state.memory[(addr/4) + i];
+            }
+        }
+        return SYS_CACHE[index_bits].data[byte_offset];
         /// @students: your implementation must properly increment: arch_state_ptr->mem_stats.lw_cache_hits
     }
     return 0;
@@ -51,8 +101,17 @@ void memory_write(int address, int write_data){
         arch_state.memory[address / 4] = (uint32_t) write_data;
     }else{
         // CACHE ENABLED
-        assert(0); /// @students: Remove assert(0); and implement Memory hierarchy w/ cache
+        int byte_offset = get_piece_of_a_word(address, 0, OFFSET_BITS) / 4;
+        int index_bits = get_piece_of_a_word(address, OFFSET_BITS, INDEX_BITS);
+        int tag = get_piece_of_a_word(address, OFFSET_BITS+INDEX_BITS, TAG_BITS);
+        int addr = 0xFFFFFFF0 & address;
 
+        if (SYS_CACHE[index_bits].valid == 1 && SYS_CACHE[index_bits].tag == tag) {
+            arch_state.mem_stats.sw_cache_hits += 1;
+            for(int i = 0; i < CACHE_BLOCK_SIZE; i++) {
+                SYS_CACHE[index_bits].data[i] = (int) arch_state.memory[(addr/4) + i];
+            }
+        }
         /// @students: your implementation must properly increment: arch_state_ptr->mem_stats.sw_cache_hits
     }
 }
